3ee286e0728f1e062cfb882306be0ef30ec311db
Tarun Maharshi <71956436+trunmhrshi@users.noreply.github.com>
2025-08-26 13:44:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,118 @@
 terraform {
   required_version = ">= 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Random suffix to avoid name conflicts
+resource "random_id" "main" {
+  byte_length = 4
+}
+
+# Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = "devops-demo-rg-${random_id.main.hex}"
+  location = "East US"
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "main" {
+  name                = "devops-vnet-${random_id.main.hex}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# Subnet
+resource "azurerm_subnet" "main" {
+  name                 = "devops-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Public IP
+resource "azurerm_public_ip" "main" {
+  name                = "devops-pip-${random_id.main.hex}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# NIC
+resource "azurerm_network_interface" "main" {
+  name                = "devops-nic-${random_id.main.hex}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+# SSH Key
+resource "tls_private_key" "main" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.main.private_key_pem
+  filename        = "${path.module}/ssh-key"
+  file_permission = "0600"
+}
+
+# VM
+resource "azurerm_linux_virtual_machine" "main" {
+  name                = "devops-vm-${random_id.main.hex}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B2s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.main.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+}
+
+# Outputs
+output "vm_public_ip" {
+  value = azurerm_public_ip.main.ip_address
+}
+
+output "ssh_connection_command" {
+  value = "ssh -i ssh-key azureuser@${azurerm_public_ip.main.ip_address}"
 }
